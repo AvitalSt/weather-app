ad5770653a652e16ea4103851ff3934d08476059
AvitalSt <154595696+AvitalSt@users.noreply.github.com>
2024-11-24 22:29:56 +0200

    update

--- weather-app-backend/test/weatherController.test.js
+++ weather-app-backend/test/weatherController.test.js
@@ -23,7 +23,7 @@ describe('getWeatherByCity', () => {
         };
         await getWeatherByCity(req, res);
         expect(axios.get).toHaveBeenCalledWith(
-            `http://api.openweathermap.org/data/2.5/weather?q=Tel Aviv&appid=${process.env.JWT_SECRET}&units=metric`
+            `http://api.openweathermap.org/data/2.5/weather?q=Tel Aviv&appid=${process.env.apiKey}&units=metric`
         );
         expect(res.status).toHaveBeenCalledWith(200);
         expect(res.json).toHaveBeenCalledWith({
